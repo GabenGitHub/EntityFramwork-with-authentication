393a1382f0cc83da4e28bbdd42e188f9dc75943c
Gabor Szabo <gabor.szabo86@hotmail.com>
2020-09-21 19:03:52 +0200

    Implement update character

--- Services/CharacterService/ICharacterService.cs
+++ Services/CharacterService/ICharacterService.cs
@@ -12,5 +12,7 @@ namespace EFwebapi.Services.CharacterService
         Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id);
 
         Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter);
+
+        Task<ServiceResponse<GetCharacterDto>> UpdateCharacter(UpdateCharacterDto updateCharacter);
     }
 }
